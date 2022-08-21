4741fd8eeb00dd3c8e8b03ee5b12e5163ee91dba
Vishesh Jindal <vishesh.jindal@nagarro.com>
2022-08-21 21:31:00 +0530

    Index

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render("index", { title: "Express", env: process.env });
+  res.render("index", { title: "NAGP", env: process.env });
 });
 
 module.exports = router;
