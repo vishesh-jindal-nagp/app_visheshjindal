19d9be739e25f1ba06a947a4651efedb1768465c
Vishesh Jindal <vishesh.jindal@nagarro.com>
2022-08-21 11:44:57 +0530

    Modified Jenkins file

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render("index", { title: "Express", env: process.env });
+  res.render("index", { title: "Express", env: process.env, branch:"develop" });
 });
 
 module.exports = router;
