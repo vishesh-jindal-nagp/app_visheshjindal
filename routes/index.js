ee15aaaae2588469bd5c12ba8d68513028dc35af
Vishesh Jindal <vishesh.jindal@nagarro.com>
2022-08-21 13:52:34 +0530

    Node Changes

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render("index", { title: "Express", env: process.env, branch:"develop" });
+  res.render("index", { title: "Express", env: process.env });
 });
 
 module.exports = router;
